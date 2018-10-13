8410bedbc5139f339b4e61987522051064a36120
Hyeong Chang Lee <hc9904@naver.com>
2018-10-14 01:10:04 +0900

    Update package.json

---
+++ docs/precache-manifest.985a86abcf91ec230b40cb6191585a95.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3a94a65f021a4d63103c",
+    "url": "/JumpToOpenSource/static/js/runtime~main.3a94a65f.js"
+  },
+  {
+    "revision": "a2fc5c28d914bb2f8f43",
+    "url": "/JumpToOpenSource/static/js/main.a2fc5c28.chunk.js"
+  },
+  {
+    "revision": "9443225b3a928e724710",
+    "url": "/JumpToOpenSource/static/js/1.9443225b.chunk.js"
+  },
+  {
+    "revision": "a2fc5c28d914bb2f8f43",
+    "url": "/JumpToOpenSource/static/css/main.9e738eed.chunk.css"
+  },
+  {
+    "revision": "b42c2af4ba70c7c0af5fb1d3cbded12b",
+    "url": "/JumpToOpenSource/index.html"
+  }
+];
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81bb461221b644d65b57cc33cb52f349.js"
+  "/JumpToOpenSource/precache-manifest.985a86abcf91ec230b40cb6191585a95.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/JumpToOpenSource/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
